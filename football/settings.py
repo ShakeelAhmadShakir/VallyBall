5bb3bdf94d36ce5c71bafce7902d01ce5a155fa8
ShakeelAhmadShakir <25852@riphahfsd.edu.pk>
2025-05-15 08:30:25 +0500

    remove whitenoise

--- football/settings.py
+++ football/settings.py
@@ -35,18 +35,18 @@ INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
-    'whiteNoise.runserver.nostatic',
     'django.contrib.sessions',
     'django.contrib.messages',
+    # 'whitenoise.runserver.nostatic',
     'django.contrib.staticfiles',
     'myapp',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
-    "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
@@ -126,3 +126,4 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, 'myapp', 'static')]
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
